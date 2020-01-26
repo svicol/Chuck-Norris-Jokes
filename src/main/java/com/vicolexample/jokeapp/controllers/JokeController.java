1e64a982104fb364f44ecf479b95145a81a27af7
svicol <vicols235@gmail.com>
2020-01-26 15:33:55 +0200

    Create Spring MVC Jokes controller. Closes #4

---
+++ src/main/java/com/vicolexample/jokeapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.vicolexample.jokeapp.controllers;
+
+import com.vicolexample.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"", "/"})
+    public String displayJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
